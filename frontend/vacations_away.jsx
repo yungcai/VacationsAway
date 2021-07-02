cad32f48655ea0deab8cd4a6322ea710a6bb5597
Yung Cai <yungcai@Yungs-MBP.fios-router.home>
2021-07-02 18:18:40 -0400

    heroku5

--- frontend/vacations_away.jsx
+++ frontend/vacations_away.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
+// import Root from './components/root';
+// import configureStore from './store/store';
 
 /*
 Write an entry point file that renders the `Root` component, with a `store`
@@ -10,6 +10,6 @@ prop passed in, inside the div with id 'root'.
 
 document.addEventListener('DOMContentLoaded', ()=> {
     const root = document.getElementById('root');
-    const store = configureStore();
-    ReactDOM.render(<Root store={store}/>, root)
+    // const store = configureStore();
+    // ReactDOM.render(<Root store={store}/>, root)
 })
