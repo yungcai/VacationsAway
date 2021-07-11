71bbe2ccb127e3abb9f928526830db4f5b963ad3
Yung Cai <yungcai09@gmail.com>
2021-07-11 15:05:57 -0400

    started some css styling

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom'
 
 
 const Splash = props => (
-    <div>
-        <Link to={`/stays`}>SPLASH</Link>
+    <div className='splash-container'>
+        <div>
+            <h1><Link className='explore-link' to={`/stays`}>EXPLORE OUR OPTIONS</Link></h1>
+            <Link className='splash-header' to={`/stays`}>FIND YOUR STAY AWAY FROM HOME</Link>
+        </div>
+
+        <div className='splash-images'>
+            <div className='splash-img1'></div>
+            <div className='splash-img2'></div>
+            <div className='splash-img3'></div>
+             <div className='splash-img4'></div>
+        </div>
     </div>
 )
 
 export default Splash
+
