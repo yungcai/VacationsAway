5622bc8d209189510303d44063f344bc5ab39587
Yung Cai <yungcai09@gmail.com>
2021-09-22 21:05:33 -0400

    new links

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -9,9 +9,9 @@ class Navbar extends React.Component {
         <div className='navbar-container'>
             <div className='logo-left'><Link to={`/`} ><img src={window.logo}/></Link></div>
             <div className='icons'>
-                <div><a href='https://www.linkedin.com/in/yung-cai-8b8744218/'><span className="iconify" data-icon="akar-icons:linkedin-fill" ></span></a></div>
-                <div><a href='https://angel.co/u/yung-cai'><span className="iconify" data-icon="cib:angellist"></span></a></div>
-                <div><a href='https://github.com/yungcai'><span className="iconify" data-icon="akar-icons:github-fill"  ></span></a></div>
+                <div><a href='https://www.linkedin.com/in/yung-cai-8b8744218/' target='_blank'><span className="iconify" data-icon="akar-icons:linkedin-fill" ></span></a></div>
+                <div><a href='https://angel.co/u/yung-cai' target='_blank'><span className="iconify" data-icon="cib:angellist"></span></a></div>
+                <div><a href='https://github.com/yungcai' target='_blank'><span className="iconify" data-icon="akar-icons:github-fill"  ></span></a></div>
             </div>
             <div className='nav-right'>
                  <GreetingContainer/>
